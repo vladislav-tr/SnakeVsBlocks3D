7e617368388b4133db0049d4376bbb79a7e79e7a
vladislav-tr <bajiehok42@yandex.ru>
2022-12-22 17:01:19 +0300

    Added player controls

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody rb;
-    private Vector3 direction;
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        direction = new Vector3(0,0,2f);
-    }
+    private Vector3 newPosition;
+    private float prevMousePosition;
     private void FixedUpdate()
     {
-        rb.velocity = direction;
+        newPosition = transform.position;
+        newPosition.z += 2f * Time.fixedDeltaTime;
+
+        if (Input.GetMouseButton(0))
+        {
+            float delta = (Input.mousePosition.x - prevMousePosition) / 100;
+            newPosition.x = Mathf.Clamp(transform.position.x + delta, -2.18f, 2.18f);
+        }
+
+        transform.position = newPosition;
+        prevMousePosition = Input.mousePosition.x;
     }
 }
